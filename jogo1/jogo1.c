71b7030bbd1e46347509c600b82cc6c3f55bbd77
dekazee <artfelipe6205@gmail.com>
2026-08-21 11:09:02 +0000

    criando o começo de um tabuleiro naval

---
+++ jogo1/jogo1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAMANHO 10
+
+void escolhaAletoria(char tC[][], int n);
+
+int main() {
+    char tabuleiroJogador[TAMANHO][TAMANHO];
+    char tabuleiroComputador[TAMANHO][TAMANHO];
+    char ataquesJogador[TAMANHO][TAMANHO];
+    int escolha[TAMANHO][TAMANHO];
+
+    printf("Escolha a sua jogada: ");
+    for(int i=0; i < TAMANHO; i++) {
+        for(int j=0; j < TAMANHO; j++) {
+
+
+        }
+    }
+
+
+
+
+}
+
+void escolhaAletoria(char tC[][], int n) {
+    int main () {
+        srand(time(NULL));
+
+
+    int r = rand(); % 100;
+    printf("numero: %d/n", r);
+    return 0;
+    }    
+    for(int i=0; i < n; i++) {
+        for(int j=0; j < n; i++) {
+
+        }
+    }
+
+}
