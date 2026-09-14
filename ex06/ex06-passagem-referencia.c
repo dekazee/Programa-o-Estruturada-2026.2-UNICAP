a472b3d707cfe8947ab57c0ad40b5f239749f761
dekazee <artfelipe6205@gmail.com>
2026-09-14 12:00:23 +0000

    ponteiros

---
+++ ex06/ex06-passagem-referencia.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void troca(int *a, int *b);
+
+int main() {
+    int numero1 = 10, numero2 = 20;
+    printf("numero1 = %d e numero2 = %d\n", numero1,numero2);
+    troca(&numero1,&numero2);
+    printf("numero1 = %d e numero2 = %d\n", numero1, numero2);
+
+}
+
+void troca(int *a, int *b) {
+    printf("*a = %d e *b = %d\n", *a, *b);
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+    printf("*a = %d e *b = %d\n", *a, *b);
+}
